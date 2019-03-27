2c247fcf24e4fc6c731badbd6365bdce5117e664
Carlo Ruiz <carloruiz.57@gmail.com>
2019-03-27 00:17:23 -0700

    config typo

--- config.py
+++ config.py
@@ -15,12 +15,12 @@ config = {
     'DB_NAME'           : 'slideshare',
     'DB_USER'           : 'csr2131',
     'DB_HOST'           : 'slideshare-prod.cshgvdefxewm.us-east-1.rds.amazonaws.com',
-    'DB_URI_PRODUCTION' : 'postgresql://{}:{}@{}:5432/{}'.format(
-                                        config["DB_USER"], os.environ['DB_PASSWORD'], 
-                                        config["DB_HOST"], config["DB_NAME"]),
     'DB_URI_LOCAL'      :  'postgresql://carloruiz@localhost:5432/slideshare'
 }
 
+config['DB_URI_PRODUCTION'] = 'postgresql://{}:{}@{}:5432/{}'.format(
+                                    config["DB_USER"], os.environ['DB_PASSWORD'], 
+                                    config["DB_HOST"], config["DB_NAME"])
 config['ORIGIN'] = config['PRODUCTION_ORIGIN'] if config['PRODUCTION'] else config['DEV_ORIGIN'] 
 config['DB_URI'] = config['DB_URI_PRODUCTION'] if config['PRODUCTION'] else config['DB_URI_LOCAL']
 
