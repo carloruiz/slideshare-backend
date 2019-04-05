9615e03afa45790ed8af60c7aeb21aea5f8e567a
Carlo Ruiz <carloruiz.57@gmail.com>
2019-04-05 14:54:07 -0400

    remove extra logging

--- slideshare/__init__.py
+++ slideshare/__init__.py
@@ -12,17 +12,12 @@ aioapp = make_aiohttp_app(app)
 from . import routes
 from . import db
 
-@app.before_request
-def log_request():
-    print(json.dumps(request.__dict__, default=lambda o: '<not serializable>', indent=2))
-
 
 @app.after_request
 def add_cors_heaer(response):
     response.headers['Access-Control-Allow-Origin'] = config['ORIGIN']
     response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
     response.headers['Access-Control-Allow-Credentials'] = "true"
-    print(response)
     return response
 
 
